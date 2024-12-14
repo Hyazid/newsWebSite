488991f9b6222639bfd65259f0f3a783a1e11e70
Hyazid <yazid.hanniche.me@gmail.com>
2024-12-14 03:16:21 -0800

    user authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("age",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("age",)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
